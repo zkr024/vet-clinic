747174981d757e39d0a8f8928ed3e3291b534050
zkr024 <zkr.bm.02403@gmail.com>
2022-06-06 15:25:01 -0500

    Update name length

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
     id int PRIMARY KEY NOT NULL,
-    name varchar() NOT NULL,
+    name varchar(100) NOT NULL,
     date_of_birth date NOT NULL,
     escape_attempts int NOT NULL,
     neutered boolean NOT NULL,
