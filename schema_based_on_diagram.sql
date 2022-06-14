099b273b1e4073d98acef011a44ccfdc25c6cbe9
zkr024 <zkr.bm.02403@gmail.com>
2022-06-14 15:14:48 -0500

    create database from the diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,82 @@
+CREATE TABLE "medical_histories"(
+    "id" INTEGER NOT NULL,
+    "admitted_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "patient_id" INTEGER NOT NULL,
+    "status" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "medical_histories" ADD PRIMARY KEY("id");
+
+CREATE TABLE "invoice_items"(
+    "id" INTEGER NOT NULL,
+    "unit_price" DECIMAL(8, 2) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "total_price" DECIMAL(8, 2) NOT NULL,
+    "invoice_id" INTEGER NOT NULL,
+    "treatment_id" INTEGER NOT NULL
+);
+ALTER TABLE
+    "invoice_items" ADD PRIMARY KEY("id");
+
+CREATE TABLE "patients"(
+    "id" INTEGER NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "date_of_birth" DATE NOT NULL
+);
+ALTER TABLE
+    "patients" ADD PRIMARY KEY("id");
+
+CREATE TABLE "invoices"(
+    "id" INTEGER NOT NULL,
+    "total_amount" DECIMAL(8, 2) NOT NULL,
+    "generated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "payed_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "medical_history_id" INTEGER NOT NULL
+);
+ALTER TABLE
+    "invoices" ADD PRIMARY KEY("id");
+
+CREATE TABLE "treatments"(
+    "id" INTEGER NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "treatments" ADD PRIMARY KEY("id");
+
+--ADD FOREIGN KEYS AND INDEXES
+
+ALTER TABLE
+    "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
+
+CREATE UNIQUE INDEX patient_id_index ON  medical_histories(patient_id);
+
+
+ALTER TABLE
+    "invoice_items" ADD CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
+
+CREATE UNIQUE INDEX treatment_id_index ON  invoice_items(treatment_id);
+
+ALTER TABLE
+    "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+
+CREATE UNIQUE INDEX invoice_id_index ON  invoice_items(invoice_id);
+
+ALTER TABLE
+    "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+
+CREATE UNIQUE INDEX medical_history_id_index ON  invoices(medical_history_id);
+
+
+-- Join tables from many-to-many relationships
+CREATE TABLE "joinTable" (
+    medical_histories_id int,
+    treatment_id int,
+    foreign key(medical_histories_id) references medical_histories(id),
+    foreign key(treatment_id) references treatments(id),
+    primary key(medical_histories_id, treatment_id)
+);
+
+
+--SELECT * FROM owners where email = 'owner_18327@mail.com'; -Execution time 168.694 ms 139.164 ms
+create index email_index on owners(email);
